26b6107c17210e7619c1e1eee2331f30d983c797
fabiana-rsl <51968537+fabiana-rsl@users.noreply.github.com>
2020-01-25 11:42:56 -0300

    Adicionar arquivos de projeto.

---
+++ LereEscreverExcel/MinhasClasses/Livro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LereEscreverExcel.MinhasClasses
+{
+    public class Livro
+    {
+        public string Titulo { get; set; }
+
+        public int ISBN { get; set; }
+
+    }
+}
---
+++ LereEscreverExcel/Program.cs
@@ -0,0 +1,67 @@
+﻿
+using LereEscreverExcel.MinhasClasses;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace LereEscreverExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Livro livro = new Livro();
+            livro.Titulo = "Biblia Sagrada";
+            livro.ISBN = 1111;
+
+            List<Livro> Lista = new List<Livro>();
+
+            Lista.Add(livro) ;
+
+            Livro livro2 = new Livro();
+            livro2.Titulo = "Harry Potter";
+            livro2.ISBN = 222;
+            Lista.Add(livro2);
+
+            Livro livro3 = new Livro();
+            livro3.Titulo = "O Pequeno Príncipe";
+            livro3.ISBN = 333;
+            Lista.Add(livro3);
+
+            Gerar(Lista);
+            //Add é método, método indica uma ação; título é uma propriedade
+
+        }
+
+        static void LerExcel()
+        {
+
+        }
+        public static void Gerar(List<Livro> livros)
+        {
+            // criando/abrindo o arquivo:
+            FileInfo caminhoNomeArquivo = new FileInfo(@"C:\Fabiana\teste.xlsx");
+            ExcelPackage arquivoExcel = new ExcelPackage(caminhoNomeArquivo);
+
+            // CRIANDO (ADD) uma planilha neste arquivo e obtendo a referência para meu código operá-la.
+            ExcelWorksheet planilha = arquivoExcel.Workbook.Worksheets.Add("Livros");
+
+            // (operações para gerar o arquivo)
+            // ESCREVENDO O CABECALHO
+            // Uma forma de escrever: informando endereco da celula
+            planilha.Cells["A1"].Value = livros[0].Titulo;
+            planilha.Cells["B1"].Value = livros[0].ISBN;
+            planilha.Cells["A2"].Value = livros[1].Titulo;
+            planilha.Cells["B2"].Value = livros[1].ISBN;
+            planilha.Cells["A3"].Value = livros[2].Titulo;
+            planilha.Cells["B3"].Value = livros[2].ISBN;
+
+
+
+            // salvando e fechando o arquivo: MUITO IMPORTANTE HEIN!!!
+            arquivoExcel.Save();
+            arquivoExcel.Dispose();
+        }
+    }
+}
